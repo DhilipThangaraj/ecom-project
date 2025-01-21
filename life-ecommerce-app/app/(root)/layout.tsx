428b99fa4609e53ab7dbaf0276ab41d6b747acdc
dhilipkumar thangaraj <dhilipthangaraj777@gmail.com>
2025-01-21 11:21:07 +0400

    added footer

--- life-ecommerce-app/app/(root)/layout.tsx
+++ life-ecommerce-app/app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/footer";
 import Header from "@/components/shared/header";
 
 //Header and Footer Layout
@@ -11,6 +12,7 @@ export default function RootLayout({
     <div className="flex h-screen flex-col">
       <Header />
       <main className="flex-1 wrapper">{children}</main>
+      <Footer />
     </div>
   );
 }
