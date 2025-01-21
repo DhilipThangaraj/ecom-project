07a1f74d23b74f075bdcb3373687c6ab39ba6f65
dhilipkumar thangaraj <dhilipthangaraj777@gmail.com>
2025-01-21 11:14:53 +0400

    added header with logo and with cart and signin button

--- life-ecommerce-app/app/(root)/layout.tsx
+++ life-ecommerce-app/app/(root)/layout.tsx
@@ -1,4 +1,7 @@
+import Header from "@/components/shared/header";
+
 //Header and Footer Layout
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -6,6 +9,7 @@ export default function RootLayout({
 }>) {
   return (
     <div className="flex h-screen flex-col">
+      <Header />
       <main className="flex-1 wrapper">{children}</main>
     </div>
   );
